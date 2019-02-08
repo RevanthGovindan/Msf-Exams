0e0f9d030c2ed995fac326ce68e9019c5ac53df2
Revanth G <revanth.g@msil.ad>
2019-02-08 19:00:34 +0530

    added sets API

--- src/main/java/com/msf/exams/controller/QuestionSetsController.java
+++ src/main/java/com/msf/exams/controller/QuestionSetsController.java
@@ -11,9 +11,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,9 +23,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.msf.exams.model.Question;
 import com.msf.exams.model.QuestionSets;
 import com.msf.exams.repository.QuestionSetsRepo;
 import com.msf.exams.response.InsertSetRes;
+import com.msf.exams.response.QuestionRes;
 
 @RestController
 @RequestMapping("/")
@@ -35,7 +39,7 @@ public class QuestionSetsController {
 	@Autowired
 	MongoTemplate mongotemplate;
 	private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
-	@PostMapping("/createset")
+	@PostMapping("/sets/createset")
 	public ResponseEntity<InsertSetRes> addQuestionSet(@RequestBody QuestionSets questionsets) {
 		InsertSetRes response = new InsertSetRes();
 		response.message = "inserted";
@@ -43,11 +47,9 @@ public class QuestionSetsController {
 		return new ResponseEntity<InsertSetRes>(response, HttpStatus.CREATED);
 	}
 
-	@GetMapping("/listsets")
+	@GetMapping("/sets/list")
 	public ResponseEntity<Object> listsets() {
 		Query query = new Query();
-		query.fields().include("setname");
-		query.fields().include("id");
 		List<QuestionSets> qsets = new ArrayList<QuestionSets>();
 		qsets = questionsetsrepo.findAll();
 		List<Object> resultList = new ArrayList<>();
@@ -58,21 +60,68 @@ public class QuestionSetsController {
 				map.put("setname", res.getSetname());
 				resultList.add(map);
 			}
-
 		} catch (Exception e) {
 			LOGGER.info(e.toString());
 		}
-
 		return new ResponseEntity<Object>(resultList, HttpStatus.OK);
 	}
 	
-	@GetMapping("/listsetquestions/{id}")
+	@GetMapping("/sets/listquestions/{id}")
 	public ResponseEntity<Object> listsetquestions(@PathVariable String id){
 		Query query = new Query();
 		query.addCriteria(Criteria.where("id").is(id));
 		QuestionSets qlists = new QuestionSets();
 		qlists = mongotemplate.findOne(query, QuestionSets.class);
-		LOGGER.info(qlists.toString());
 		return new ResponseEntity<Object>(qlists, HttpStatus.OK);
 	}
+	 
+	@DeleteMapping("/sets/deletequestion/{setid}/{questionid}")
+	public ResponseEntity<Object> deleteSetQuestions(@PathVariable String setid,@PathVariable String questionid){
+		Query query = new Query();
+		QuestionRes response = new QuestionRes();
+		query.addCriteria(Criteria.where("id").is(setid));
+		Update update = new Update();
+		update.pull("questions", new Query(Criteria.where("id").is(questionid)));
+		if(mongotemplate.updateFirst(query,update, QuestionSets.class) != null){
+			response.message = "deleted";
+		} else {
+			response.message = "failed";
+		}
+		
+		return new ResponseEntity<Object>(response, HttpStatus.OK);
+	}
+	
+	@DeleteMapping("/sets/deleteset/{setid}")
+	public ResponseEntity<Object> deleteSet(@PathVariable String setid){
+		QuestionRes response = new QuestionRes();
+		Query query = new Query();
+		query.addCriteria(Criteria.where("id").is(setid));
+		if(mongotemplate.remove(query,  QuestionSets.class) != null) {
+			response.message = "deleted";
+		} else {
+			response.message = "failed";
+		}		
+		return new ResponseEntity<Object>(response, HttpStatus.OK);
+	}
+	
+	@PostMapping("/sets/addquestions")
+	public ResponseEntity<Object> addQuestions(@RequestBody QuestionSets request){
+		QuestionRes response = new QuestionRes();
+		Update update = new Update();
+		List<Question> questions = new ArrayList<Question>();
+		questions = request.getQuestions() ;
+		String setid = request.getId();
+		Query query = new Query();
+		LOGGER.info(setid);
+		query.addCriteria(Criteria.where("id").is(setid));
+		update.push("questions").each(questions);
+//		for(Question question:questions) {
+//			LOGGER.info(question.toString());
+//			update.add ("questions", question);
+//			
+//		}	
+		mongotemplate.updateFirst(query, update, QuestionSets.class);
+		return new ResponseEntity<Object>(response, HttpStatus.OK);
+	}
+	
 }
